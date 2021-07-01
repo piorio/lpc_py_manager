e7bd0fa6d017d387e543fa31b5780901738ce339
Paolo Iorio <piorio@soldo.com>
2021-07-01 18:02:02 +0200

    Add all main roster domain objects

--- LpcManager/admin.py
+++ LpcManager/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Race, Skill, Trait, RosterPlayer
+from .models import Race, Skill, Trait, RosterPlayer, RosterTeam
+
 
 # Register your models here.
 admin.site.register(Race)
 admin.site.register(Skill)
 admin.site.register(Trait)
 admin.site.register(RosterPlayer)
+admin.site.register(RosterTeam)
--- LpcManager/models.py
+++ LpcManager/models.py
@@ -1,11 +1,15 @@
 from django.db import models
-from django.db.models import IntegerField, Model
+from django.db.models import Model
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Race(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 
 class Skill(models.Model):
@@ -13,10 +17,29 @@ class Skill(models.Model):
     description = models.TextField(max_length=255, null=True, blank=True, default=None)
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 
 class Trait(models.Model):
     description = models.TextField(max_length=255, null=True, blank=True, default=None)
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class RosterTeam(models.Model):
+    apothecary = models.BooleanField(default=False)
+    name = models.CharField(max_length=50)
+    re_roll_cost = models.IntegerField(default=0, validators=[MaxValueValidator(1000000), MinValueValidator(0)])
+    re_roll_max = models.IntegerField(default=0, validators=[MaxValueValidator(16), MinValueValidator(0)])
+    special_rules = models.CharField(max_length=50, null=True, blank=True, default=None)
+    tier = models.IntegerField(default=0, validators=[MaxValueValidator(5), MinValueValidator(0)])
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 
 class RosterPlayer(models.Model):
@@ -29,6 +52,11 @@ class RosterPlayer(models.Model):
     passing = models.IntegerField(default=0, validators=[MaxValueValidator(10), MinValueValidator(0)])
     position = models.CharField(max_length=50)
     strength = models.IntegerField(default=0, validators=[MaxValueValidator(10), MinValueValidator(0)])
-    primary_skills = models.ManyToManyField(Skill, related_name='primary_skills')
-    secondary_skills = models.ManyToManyField(Skill, related_name='secondary_skills')
-    traits = models.ManyToManyField(Trait)
+    primary_skills = models.ManyToManyField(Skill, related_name='primary_skills', blank=True)
+    secondary_skills = models.ManyToManyField(Skill, related_name='secondary_skills', blank=True)
+    skills = models.ManyToManyField(Skill, related_name='skills', blank=True)
+    traits = models.ManyToManyField(Trait, blank=True)
+    roster_team = models.ForeignKey(RosterTeam, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.position
