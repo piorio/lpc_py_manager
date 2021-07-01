5f0ae5ebd0a65d07e6fa761e8e622d42dbbf3184
Paolo Iorio <piorio@soldo.com>
2021-07-01 23:51:07 +0200

    Add login/logout functionality Add home page

--- lpc_py_manager/settings.py
+++ lpc_py_manager/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'LpcManager.apps.LpcmanagerConfig',
 ]
 
@@ -125,3 +126,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# config/settings.py
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
