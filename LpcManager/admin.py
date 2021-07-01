122d8865857cea79bd465f704f52b2ca4e74a832
Paolo Iorio <piorio@soldo.com>
2021-07-01 17:08:58 +0200

    Add first models

--- LpcManager/admin.py
+++ LpcManager/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Race, Skill, Trait, RosterPlayer
 
 # Register your models here.
+admin.site.register(Race)
+admin.site.register(Skill)
+admin.site.register(Trait)
+admin.site.register(RosterPlayer)
--- LpcManager/models.py
+++ LpcManager/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.db.models import IntegerField, Model
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
+class Race(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Skill(models.Model):
+    category = models.CharField(max_length=50)
+    description = models.TextField(max_length=255, null=True, blank=True, default=None)
+    name = models.CharField(max_length=50)
+
+
+class Trait(models.Model):
+    description = models.TextField(max_length=255, null=True, blank=True, default=None)
+    name = models.CharField(max_length=50)
+
+
+class RosterPlayer(models.Model):
+    agility = models.IntegerField(default=0, validators=[MaxValueValidator(10), MinValueValidator(0)])
+    armor_value = models.IntegerField(default=0, validators=[MaxValueValidator(10), MinValueValidator(0)])
+    cost = models.IntegerField(default=0, validators=[MaxValueValidator(1000000), MinValueValidator(0)])
+    max_quantity = models.IntegerField(default=0, validators=[MaxValueValidator(16), MinValueValidator(0)])
+    min_quantity = models.IntegerField(default=0, validators=[MaxValueValidator(16), MinValueValidator(0)])
+    movement_allowance = models.IntegerField(default=0, validators=[MaxValueValidator(20), MinValueValidator(0)])
+    passing = models.IntegerField(default=0, validators=[MaxValueValidator(10), MinValueValidator(0)])
+    position = models.CharField(max_length=50)
+    strength = models.IntegerField(default=0, validators=[MaxValueValidator(10), MinValueValidator(0)])
+    primary_skills = models.ManyToManyField(Skill, related_name='primary_skills')
+    secondary_skills = models.ManyToManyField(Skill, related_name='secondary_skills')
+    traits = models.ManyToManyField(Trait)
