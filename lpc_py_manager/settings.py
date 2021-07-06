f21808b0d2bd8500cb2240e247f5299dd3821d85
Paolo Iorio <piorio@soldo.com>
2021-07-06 10:21:42 +0200

    Hide secret key

--- lpc_py_manager/settings.py
+++ lpc_py_manager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from secret_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$49-mp_q6c%&05!!1&zgx28yhvbwg-zu-pvz!0!s$wg9ylazni'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
