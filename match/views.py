fdf6f009fe3ac3f89b555957fd480927e6b89650
Paolo Iorio <piorio@soldo.com>
2021-08-16 18:32:13 +0200

    Close match done

--- match/views.py
+++ match/views.py
@@ -2,10 +2,12 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
 from django.http import HttpResponse
-from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView
 from match.models import Match
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import CloseMatchForm
+from django.contrib import messages
 
 
 # Create your views here.
@@ -41,7 +43,59 @@ class MyMatchesListView(LoginRequiredMixin, ListView):
 def close_match(request, match_id):
     if not request.user.is_superuser:
         return HttpResponse(status=403)  # HTTP 403 Forbidden
+
+    match = get_object_or_404(Match, id=match_id)
+
     if request.method == 'POST':
-        pass
+        match_form = CloseMatchForm(request.POST)
+        if match_form.is_valid():
+            match.first_team_td = match_form.cleaned_data['first_team_td']
+            match.second_team_td = match_form.cleaned_data['second_team_td']
+            match.first_team_badly_hurt = match_form.cleaned_data['first_team_badly_hurt']
+            match.second_team_badly_hurt = match_form.cleaned_data['second_team_badly_hurt']
+            match.first_team_serious_injury = match_form.cleaned_data['first_team_serious_injury']
+            match.second_team_serious_injury = match_form.cleaned_data['second_team_serious_injury']
+            match.first_team_kill = match_form.cleaned_data['first_team_kill']
+            match.second_team_kill = match_form.cleaned_data['second_team_kill']
+
+            first_team_cas = 0
+            second_team_cas = 0
+
+            if match.first_team_badly_hurt is not None:
+                first_team_cas += match.first_team_badly_hurt
+            if match.first_team_serious_injury is not None:
+                first_team_cas += match.first_team_serious_injury
+            if match.first_team_kill is not None:
+                first_team_cas += match.first_team_kill
+            match.first_team_cas = first_team_cas
+
+            if match.second_team_badly_hurt is not None:
+                second_team_cas += match.second_team_badly_hurt
+            if match.second_team_serious_injury is not None:
+                second_team_cas += match.second_team_serious_injury
+            if match.second_team_kill is not None:
+                second_team_cas += match.second_team_kill
+            match.second_team_cas = second_team_cas
+
+            match.save()
+            return redirect('match:all_matches')
+        else:
+            messages.error(request, 'Close error ' + str(match_form.errors))
+            match_form = CloseMatchForm(initial={
+                'first_team_td': match.first_team_td, 'second_team_td': match.second_team_td,
+                'first_team_badly_hurt': match.first_team_badly_hurt,
+                'first_team_serious_injury': match.second_team_serious_injury,
+                'first_team_kill': match.first_team_kill, 'second_team_badly_hurt': match.second_team_badly_hurt,
+                'second_team_serious_injury': match.second_team_serious_injury,
+                'second_team_kill': match.second_team_kill
+            })
+            return render(request, 'matches/close_match.html', {'match': match, 'form': match_form})
     else:
-        return render(request, 'matches/close_match.html', {})
+        match_form = CloseMatchForm(initial={
+            'first_team_td': match.first_team_td, 'second_team_td': match.second_team_td,
+            'first_team_badly_hurt': match.first_team_badly_hurt,
+            'first_team_serious_injury': match.first_team_serious_injury,
+            'first_team_kill': match.first_team_kill, 'second_team_badly_hurt': match.second_team_badly_hurt,
+            'second_team_serious_injury': match.second_team_serious_injury, 'second_team_kill': match.second_team_kill
+        })
+        return render(request, 'matches/close_match.html', {'match': match, 'form': match_form})
