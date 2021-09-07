0d201f958a4c352ee686b4266c9ec2553d164616
Paolo Iorio <piorio@soldo.com>
2021-09-07 18:20:49 +0200

    Done close match: worst code ever written but works.

--- match/forms.py
+++ match/forms.py
@@ -1,13 +1,2 @@
 from django import forms
 
-
-class CloseMatchForm(forms.Form):
-    first_team_td = forms.IntegerField(min_value=0, max_value=100)
-    second_team_td = forms.IntegerField(min_value=0, max_value=100)
-    first_team_badly_hurt = forms.IntegerField(min_value=0, max_value=100, required=False)
-    first_team_serious_injury = forms.IntegerField(min_value=0, max_value=100, required=False)
-    first_team_kill = forms.IntegerField(min_value=0, max_value=100, required=False)
-    second_team_badly_hurt = forms.IntegerField(min_value=0, max_value=100, required=False)
-    second_team_serious_injury = forms.IntegerField(min_value=0, max_value=100, required=False)
-    second_team_kill = forms.IntegerField(min_value=0, max_value=100, required=False)
-
