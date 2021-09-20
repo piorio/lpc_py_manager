9d64fa45b3b092ebfd49a1b1e422e5bae00f8876
Paolo Iorio <piorio@soldo.com>
2021-09-20 14:01:17 +0200

    Update UI for my team detail

--- static/js/demo/datatables-demo.js
+++ static/js/demo/datatables-demo.js
@@ -1,4 +1,6 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#dataTable').DataTable({
+    "lengthMenu": [[16, 25, 50, -1], [16, 25, 50, "All"]]
+  });
 });
